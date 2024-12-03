f2dc835431477969d441db778a2310ef70698316
Guilherme <guimendesmen124@gmail.com>
2024-12-03 00:24:26 -0300

    Delete database directory

--- database/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUser = 'root';
-    $dbPassword = '';
-    $dbName = 'techwarejp';
-
-    $conexao = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
-
-?>
